cf3cf3522143f4ebba830b67df211950acf8f436
agigli0 <alex.giglio@live.com>
2017-02-03 18:55:30 -0500

    fixed scaling

--- setup.js
+++ setup.js
@@ -1,10 +1,19 @@
 var canvas;
+var marginLeft = 330;
+var marginGeneral = 10;
 
 function setup() {
-
+	var w = window.innerWidth;
+	var h = window.innerHeight;
   // create canvas
-  canvas = createCanvas(960, 720);
-  canvas.position(330,10);
+  canvas = createCanvas(w - (marginLeft + marginGeneral), h - 2 * marginGeneral);
+  canvas.position(marginLeft, marginGeneral);
   background(0);
 }
 
+window.onresize = function() {
+  var w = window.innerWidth;
+  var h = window.innerHeight;  
+  canvas.size(w - (marginLeft + marginGeneral), h - 2 * marginGeneral);
+  canvas.position(marginLeft, marginGeneral);
+};
